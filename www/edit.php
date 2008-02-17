0beb158bb631994b089a3d847d849f96dd55218f
andreassolberg <andreassolberg@0bee8142-3345-0410-bf64-9fd234f9a45e>
2008-02-17 08:35:09 +0000

    adding failhandling when illegal characters in wikiid

--- www/edit.php
+++ www/edit.php
@@ -56,113 +56,127 @@ if (!isset($_SESSION['wikiplex_cachedwiki'])) {
 }
 
 
-/*
- * What wiki are we talking about?
- */
-$thiswiki = null;
-if (isset($_REQUEST['edit'])) {
-	$_SESSION['edit'] = $_REQUEST['edit'];
-	$thiswiki = $_REQUEST['edit'];
-} elseif(isset($_SESSION['edit'])) {
-	$thiswiki = $_SESSION['edit'];
-}
-if (empty($thiswiki)) throw new Exception('No wiki selected');
 
-
-
-$link = mysql_connect(
-	$config->getValue('db.host', 'localhost'), 
-	$config->getValue('db.user'),
-	$config->getValue('db.pass'));
-if(!$link){
-	throw new Exception('Could not connect to database: '.mysql_error());
-}
-mysql_select_db($config->getValue('db.name','feideopenwiki'));
-
-
-
-
-if (! array_key_exists($thiswiki,$_SESSION['wikiplex_cachedwiki'] )) {
-
-	// Create a test wiki
-	$ow = new OpenWiki($thiswiki, $link);
-	#$ow->setInfo('Test wiki', 'This is a wiki Andreas is testing', 'andreas_solberg_uninett', 3);
-
-	if (isset($_REQUEST['createnewsubmit'])) {
-		if (!$ow->isLoaded()) {
-			$ow->setOwner($username);
-		}
-		// Load from db.	
-	}
-	
-	$_SESSION['wikiplex_cachedwiki'][$thiswiki] =& $ow;
+try {
 	
 	/*
-	$ow->setInfo('Test wiki', 'This is a wiki Andreas is testing', 'andreas_solberg_uninett', 3);
-	$ow->addACL(array('@org_x_dc_uninett_no', 1));
-	$ow->addACL(array('@org_x_dc_uninett_no2', 15));
-	$ow->addACL(array('@org_x_dc_uninett_no3', 1));
-	$ow->addACL(array('@org_x_dc_uninett_no4', 0));
-	*/
+	 * What wiki are we talking about?
+	 */
+	$thiswiki = null;
+	if (isset($_REQUEST['edit'])) {
+		$_SESSION['edit'] = $_REQUEST['edit'];
+		$thiswiki = $_REQUEST['edit'];
+	} elseif(isset($_SESSION['edit'])) {
+		$thiswiki = $_SESSION['edit'];
+	}
+	if (empty($thiswiki)) throw new Exception('No wiki selected');
 	
-} else {
-
-	$ow =& $_SESSION['wikiplex_cachedwiki'][$thiswiki];
-
-}
-
-if (!empty($_REQUEST['name'])) {
-	$ow->setInfo($_REQUEST['name'], $_REQUEST['descr'], $username, $_REQUEST['access']);
-}
-if (isset($_REQUEST['aclswap'])) {
-	$ow->swapACL($_REQUEST['aclswap']);
-}
-if (isset($_REQUEST['acldelete'])) {
-	$ow->removeACL($_REQUEST['acldelete']);
-}
-if (!empty($_REQUEST['addgroupid'])) {
-	$ow->addACL($_REQUEST['addgroupid'], $_REQUEST['addgrouplevel']);
-} elseif(!empty($_REQUEST['addpersonid'])) {
-	$ow->addACL($_REQUEST['addpersonid'], $_REQUEST['addpersonlevel']);
-}
-
-
-if (isset($_REQUEST['save']) ) {
-	$ow->setDBhandle($link);
-	$ow->savetoDB();
-	unset($_SESSION['wikiplex_cachedwiki'][$thiswiki]);
 	
-	OpenWikiDirectory::writeACLdefinition($link, $config->getValue('aclfile'));
 	
-	$et = new SimpleSAML_XHTML_Template($config, 'wikisave.php');
-	$et->data['header'] = 'Wiki is successfully saved';
+	$link = mysql_connect(
+		$config->getValue('db.host', 'localhost'), 
+		$config->getValue('db.user'),
+		$config->getValue('db.pass'));
+	if(!$link){
+		throw new Exception('Could not connect to database: '.mysql_error());
+	}
+	mysql_select_db($config->getValue('db.name','feideopenwiki'));
+	
+	
+	
+	
+	if (! array_key_exists($thiswiki,$_SESSION['wikiplex_cachedwiki'] )) {
+	
+		// Create a test wiki
+		$ow = new OpenWiki($thiswiki, $link);
+		#$ow->setInfo('Test wiki', 'This is a wiki Andreas is testing', 'andreas_solberg_uninett', 3);
+	
+		if (isset($_REQUEST['createnewsubmit'])) {
+			
+			if (!preg_match('/^[a-z]+$/', $thiswiki))
+				throw new Exception('You tried to create a new wiki, but the wiki ID that you chose contained illegal characters. A wiki ID can only contain lowercase letters [a-z]!');
+		
+			if (!$ow->isLoaded()) {
+				$ow->setOwner($username);
+			}
+			// Load from db.	
+		}
+		
+		$_SESSION['wikiplex_cachedwiki'][$thiswiki] =& $ow;
+		
+		/*
+		$ow->setInfo('Test wiki', 'This is a wiki Andreas is testing', 'andreas_solberg_uninett', 3);
+		$ow->addACL(array('@org_x_dc_uninett_no', 1));
+		$ow->addACL(array('@org_x_dc_uninett_no2', 15));
+		$ow->addACL(array('@org_x_dc_uninett_no3', 1));
+		$ow->addACL(array('@org_x_dc_uninett_no4', 0));
+		*/
+		
+	} else {
+	
+		$ow =& $_SESSION['wikiplex_cachedwiki'][$thiswiki];
+	
+	}
+	
+	if (!empty($_REQUEST['name'])) {
+		$ow->setInfo($_REQUEST['name'], $_REQUEST['descr'], $username, $_REQUEST['access']);
+	}
+	if (isset($_REQUEST['aclswap'])) {
+		$ow->swapACL($_REQUEST['aclswap']);
+	}
+	if (isset($_REQUEST['acldelete'])) {
+		$ow->removeACL($_REQUEST['acldelete']);
+	}
+	if (!empty($_REQUEST['addgroupid'])) {
+		$ow->addACL($_REQUEST['addgroupid'], $_REQUEST['addgrouplevel']);
+	} elseif(!empty($_REQUEST['addpersonid'])) {
+		$ow->addACL($_REQUEST['addpersonid'], $_REQUEST['addpersonlevel']);
+	}
+	
+	
+	if (isset($_REQUEST['save']) ) {
+		$ow->setDBhandle($link);
+		$ow->savetoDB();
+		unset($_SESSION['wikiplex_cachedwiki'][$thiswiki]);
+		
+		OpenWikiDirectory::writeACLdefinition($link, $config->getValue('aclfile'));
+		
+		$et = new SimpleSAML_XHTML_Template($config, 'wikisave.php');
+		$et->data['header'] = 'Wiki is successfully saved';
+		$et->data['identifier'] = $ow->getIdentifier();
+		$et->data['name'] = $ow->getName();
+		$et->data['descr'] = $ow->getDescr();	
+		$et->show();
+		exit;
+	}
+	
+	
+	$ow->needAdminAccess($username);
+	
+	
+	#echo 'dump: <pre>' . $ow->getACLdefinition() . '</pre>';
+	
+	
+	
+	$et = new SimpleSAML_XHTML_Template($config, 'wikiedit.php');
+	$et->data['header'] = 'Edit wiki';
+	$et->data['tgroups'] = $groups;
+	$et->data['taccess'] = $access;
+	
 	$et->data['identifier'] = $ow->getIdentifier();
 	$et->data['name'] = $ow->getName();
-	$et->data['descr'] = $ow->getDescr();	
+	$et->data['descr'] = $ow->getDescr();
+	$et->data['acl'] = $ow->getCustomACL();
+	$et->data['access'] = $ow->getAccess();
+	
 	$et->show();
-	exit;
+
+} catch (Exception $e) {
+	
+	
+	SimpleSAML_Utilities::fatalError((isset($session) ? $session->getTrackID() : null), null, $e);
+
 }
-
-
-$ow->needAdminAccess($username);
-
-
-#echo 'dump: <pre>' . $ow->getACLdefinition() . '</pre>';
-
-
-
-$et = new SimpleSAML_XHTML_Template($config, 'wikiedit.php');
-$et->data['header'] = 'Edit wiki';
-$et->data['tgroups'] = $groups;
-$et->data['taccess'] = $access;
-
-$et->data['identifier'] = $ow->getIdentifier();
-$et->data['name'] = $ow->getName();
-$et->data['descr'] = $ow->getDescr();
-$et->data['acl'] = $ow->getCustomACL();
-$et->data['access'] = $ow->getAccess();
-
-$et->show();
 
 
 ?>
