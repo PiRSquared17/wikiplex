a5bc38c2458080014f8050b26aefc032dedaaf5a
andreassolberg <andreassolberg@0bee8142-3345-0410-bf64-9fd234f9a45e>
2008-05-09 06:48:06 +0000

    updated templates

--- templates/en/wikilist.php
+++ templates/en/wikilist.php
@@ -2,63 +2,87 @@
 	$this->includeAtTemplateBase('includes/header.php'); 
 ?>
 
-	<h1><?php if (isset($data['header'])) { echo $data['header']; } else { echo "Some error occured"; } ?></h1>
+	<div id="newwiki">
 
-	<p>Here is a list of existing wikis.</p>
-
-	<h2>Your own wikis (<?php echo $this->data['user']; ?>)</h2>
+	<img src="resources/dokuwiki.png" style="float: right; border: none" />
+	<h2 style="margin-top: 2px" >Create a new wiki</h2>
 	
-	<dl>
+	<form method="post" action="edit.php">
+		<input type="hidden" name="createnew" value="1" />
+		
+
+		
+		<p>The first thing you need to do to create a wiki is to select a wiki identifier, a machine readable name of the wiki. Examples of wiki identifiers: andreas, simplesamlphp, fasintegrasjon, intmask, foo. Please make the wiki identifier descriptive of the wiki content. You can not change the wiki identifier later.</p>
+		
+		
+		<p>Important: <i>The wiki identifier must consist of one or more lowercase characters [a-z]. Maximum 15 characters.</i></p>
+		
+		<p>Wiki identifier: 
+		<input type="text" name="edit" value="" /></p>
+		
+
+		
+		<input type="submit" name="createnewsubmit" value="Create new wiki" />
+		
+	</form>
+	</div>
+	
+	
+
+	<h2>Your wikis</h2>
+	
+	<p>You are authenticated as <span style="color: #833"><?php echo $this->data['user']; ?></span>, and these are wikis that you have access to administer:</p>
+	
+
+
 	<?php
 	
 		foreach ($this->data['listprivate'] AS $wiki) {
 			
-			echo '<dt>' . $wiki->getName() . '</dt>';
-			echo '<dd><p>' . $wiki->getDescr() . '</p>';
-			echo '<p>[ <a href="http://ow.feide.no/' . $wiki->getIdentifier() . ':start">visit wiki</a> 
-			| <a href="edit.php?edit=' . $wiki->getIdentifier() . '">setup</a>
-			]</p></dd>';
+			echo '<div class="wikientry">';
+			
+			echo '<h3>' . $wiki->getName() . '</h3>';
+			echo '<p>' . $wiki->getDescr() . '</p>';
+			
+			echo '<p><img class="linkicon" src="resources/web-link.png" /><a href="http://ow.feide.no/' . $wiki->getIdentifier() . ':start">Go to ' . $wiki->getName() . '</a> ';
+			echo '<img class="linkicon" src="resources/settings.png" /><a href="edit.php?edit=' . $wiki->getIdentifier() . '">Administer ' . $wiki->getName() . '</a></p>';
+			
+			echo '</div>';
 		}
 	
 	
 	?>
-	</dl>
+
 
 	
 	
 	<h2>Public wikis</h2>
 	
-	<dl>
+	<p>Public wikis is wikis that are accessible for all authenticated users, or even anonymous users. </p>
 	
 	<?php
+
 	
-		#print_r($this->data['listpublic']);
+	
 		foreach ($this->data['listpublic'] AS $wiki) {
 			
-			echo '<dt>' . $wiki->getName() . '</dt>';
-			echo '<dd><p>' . $wiki->getDescr() . '</p>';
-			echo '<p>[ <a href="http://ow.feide.no/' . $wiki->getIdentifier() . ':start">visit wiki</a> ]</p></dd>';
+			echo '<div class="wikientry">';
+			
+			echo '<h3>' . $wiki->getName() . '</h3>';
+			echo '<p>' . $wiki->getDescr() . '</p>';
+			
+			echo '<p><img class="linkicon" src="resources/web-link.png" /><a href="http://ow.feide.no/' . $wiki->getIdentifier() . ':start">Go to ' . $wiki->getName() . '</a> ';
+	#		echo '<img src="resources/settings.png" /><a href="edit.php?edit=' . $wiki->getIdentifier() . '">Administer ' . $wiki->getName() . '</a></p>';
+			
+			echo '</div>';
 		}
-	
 	
 	?>
 
 	</dl>
 	
 
-	<h2>Create a new wiki</h2>
-	
-	<form method="post" action="edit.php">
-		<input type="hidden" name="createnew" value="1" />
-		
-		<p>Wiki ID: 
-		<input type="text" name="edit" value="" /></p>
-		
-		<p>The wiki ID must consist of 1 or more lowercase characters [a-z]. Maximum 15 characters.</p>
-		
-		<input type="submit" name="createnewsubmit" value="Create new wiki" />
-		
-	</form>
+
 
 			
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
